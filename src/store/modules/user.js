316dc6f8fe48670217a7745c0c313767d5d5ae27
jburer <jburer@gmail.com>
2021-04-23 17:03:54 -0400

    User State updates

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -10,9 +10,6 @@ export const mutations = {
   },
   DISABLE_USER(state) {
     state.user = {};
-  },
-  SET_SESSION(state, session) {
-    state.session = session;
   }
 };
 
@@ -21,56 +18,14 @@ export const actions = {
     console.log("\nuser.setUser() ... start");
 
     commit("SET_USER", user);
-    /*
-    var shindig = getters.getShindigById(id);
 
-    if (shindig) {
-      commit("SET_SHINDIG", shindig);
-    } else {
-      Service.getShindig(id)
-        .then(response => {
-          commit("SET_SHINDIG", response.data);
-        })
-        .catch(error => {
-          console.log("There was an error:" + error.response);
-        });
-    }
-    */
+    console.log("\nuser.setUser() ... end");
   },
   disableUser({ commit }) {
     console.log("\nuser.disableUser() ... start");
 
     commit("DISABLE_USER");
-    /*
-    return Service.deleteShindig(shindigToDelete)
-      .then(() => {
-        commit("DELETE_SHINDIG", shindigToDelete);
-        console.log("Shindig " + shindigToDelete + " deleted.");
-      })
-      .catch(error => {
-        console.log(
-          "There was a problem deleting your shindig: " + error.message
-        );
-      });
-    */
 
     console.log("\nuser.disableUser() ... end");
-  }
-  /*
-  setSession({ commit }, session) {
-    console.log("\nuser.setSession() ... start");
-
-    console.log(" ... user.setSession(): session = " + session);
-
-    commit("SET_SESSION", session);
-
-    console.log("\nuser.setSession() ... end");
-  }
-  */
-};
-
-export const getters = {
-  getUser: state => id => {
-    return state.shindigs.find(shindig => shindig.id === id);
   }
 };
